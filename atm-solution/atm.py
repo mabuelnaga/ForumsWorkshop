438da69cc1c5ecb388c6b012a3be7cd816f76555
Mohamed Abuelnaga <m8shaheed@gmail.com>
2018-03-16 00:07:35 +0300

    ATM Solution - Functions

--- atm-solution/atm.py
+++ atm-solution/atm.py
@@ -1,32 +1,42 @@
+
+def withdraw(balance, request):
 # allowed papers: 100, 50, 10, 5, and rest of request
+    print 'Current balane = ' + str(balance)
 
-money = 500
-request = 277
+    if request > balance:
+        print "Can't give you all this money !!"
 
-if request > money:
-    print "Can't give you all this money !!"
+    elif request < 0:
+        print 'More than zero plz!!'
 
-elif request < 0:
-    print 'More than zero plz!!'
+    else:
+        new_balance = balance - request
+        while request > 0:
+            if request >= 100:
+                request -= 100
+                print 'give 100'
 
-else:
-    while request > 0:
-        if request >= 100:
-            request -= 100
-            print 'give 100'
+            elif request >= 50:
+                request -= 50
+                print 'give 50'
 
-        elif request >= 50:
-            request -= 50
-            print 'give 50'
+            elif request >= 10:
+                request -= 10
+                print 'give 10'
 
-        elif request >= 10:
-            request -= 10
-            print 'give 10'
+            elif request >= 5:
+                request -= 5
+                print 'give 5'
 
-        elif request >= 5:
-            request -= 5
-            print 'give 5'
+            elif request < 5:
+                print 'give ' + str(request)
+                request = 0
+        return new_balance
 
-        elif request < 5:
-            print 'give ' + str(request)
-            request = 0
+# Test Cases:
+balance = 500
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
