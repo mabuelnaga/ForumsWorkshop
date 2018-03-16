0c33c8111a2b6d554c8cd0a7368d17fe1d411cc3
Mohamed Abuelnaga <m8shaheed@gmail.com>
2018-03-16 20:15:59 +0300

    Forums - Entities/Models

---
+++ forums/main.py
@@ -0,0 +1,19 @@
+from models import Member, Post
+
+member1 = Member('Adam', 1)
+member2 = Member('Talia', 5)
+
+post1 = Post('Hello there!', 'My name is Mohamed')
+post2 = Post('Python', 'Python is cool!')
+
+
+
+
+# Test Cases:
+
+print member1.name
+print member2.age
+
+
+print post1.subject
+print post2.content
